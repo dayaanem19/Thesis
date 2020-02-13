d1d61cfc97c984394de989828114b087eb2f743a
dayaanem19 <dayaanem0119@gmail.com>
2020-02-14 02:58:34 +0800

    Added Switch on bluetooth functionality

--- motorcycleApp/app/src/main/java/com/example/motorcycleapp/ActivitySecond21.java
+++ motorcycleApp/app/src/main/java/com/example/motorcycleapp/ActivitySecond21.java
@@ -1,15 +1,24 @@
 package com.example.motorcycleapp;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivitySecond21 extends AppCompatActivity {
 
     private Button nextBtn;
+    private Switch bluetoothSwitch;
+    private TextView text;
+    private BluetoothAdapter myBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,39 @@ public class ActivitySecond21 extends AppCompatActivity {
             }
         });
 
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetoothAdapter == null) {
+            bluetoothSwitch.setChecked(false);
+            text.setText("Status: not supported");
+
+            //pop up message
+            Toast.makeText(getApplicationContext(),"Your device does not support Bluetooth",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            bluetoothSwitch = findViewById(R.id.bluetoothSwitch);
+            bluetoothSwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (bluetoothSwitch.isChecked()) {
+                        Log.d("SAMPLE", "Setting Switch to on!");
+
+                        if (!myBluetoothAdapter.isEnabled()) {
+                            Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+
+                            Toast.makeText(getApplicationContext(),"Bluetooth turned on" ,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"Bluetooth is already on",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Log.d("SAMPLE", "Setting Switch to off!");
+                    }
+                }
+            });
+        }
 
     }
 
